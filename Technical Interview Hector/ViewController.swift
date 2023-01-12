a04471c231501fb16df130f7a7558071ad20d47e
Hector Rodriguez <hector@jetrobotlabs.com>
2023-01-12 09:20:37 -0600

    Initial Commit

---
+++ Technical Interview Hector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technical Interview Hector
+//
+//  Created by Hector Rodriguez on 1/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
